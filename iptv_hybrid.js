5932516ff96433d2aa57ab0ddc8f98f5f6bc0df3
novyx0 <strrve@gmail.com>
2025-11-23 18:17:58 +0200

    Update iptv_hybrid.js

--- iptv_hybrid.js
+++ iptv_hybrid.js
@@ -1,50 +1,42 @@
 (function () {
     'use strict';
 
-    // 1. Конфигурация и скрытый плейлист
-    // URL жестко вшит в замыкание, пользователь его не видит и не может изменить
-    var playlist_config = {
-        id: 'ua_embedded_premium',
+    // 1. Конфигурация (Скрытая ссылка)
+    var config = {
         name: 'IPTV Premium',
-        url: 'https://tva.org.ua/ip/sam/ua.m3u', // Твоя скрытая ссылка
+        url: 'https://stantv.pro/test/pJxautaY.m3u',
         active: true
     };
 
-    // 2. Утилиты для очистки названий (из оригинального iptv.js)
-    var Utils = {
-        clearChannelName: function (name) {
-            return name.replace(/ hd$| нd$| .hd$| .нd$/gi, '')
-                       .replace(/ sd$/gi, '')
-                       .replace(/ hd | нd | .hd | .нd /gi, ' ')
-                       .replace(/ sd /gi, ' ')
-                       .trim();
-        }
-    };
-
-    // 3. Парсер M3U (Локальный, без внешних API)
+    // 2. Утилиты для M3U (Парсер)
     var Parser = {
         run: function (text) {
             var lines = text.split('\n');
             var channels = [];
-            var currentGroup = 'Інше';
+            var currentGroup = 'Общие';
 
             lines.forEach(function (line) {
                 line = line.trim();
                 if (line.startsWith('#EXTINF:')) {
                     var info = line;
+                    // Парсим атрибуты
                     var groupMatch = info.match(/group-title="([^"]+)"/i);
                     var logoMatch = info.match(/tvg-logo="([^"]+)"/i);
+                    var tvgIdMatch = info.match(/tvg-id="([^"]+)"/i);
+                    var tvgNameMatch = info.match(/tvg-name="([^"]+)"/i);
+                    
+                    // Парсим имя канала
                     var nameParts = info.split(',');
                     var name = nameParts.length > 1 ? nameParts[nameParts.length - 1].trim() : 'Channel';
 
-                    currentGroup = groupMatch ? groupMatch[1] : (currentGroup || 'Інше');
+                    currentGroup = groupMatch ? groupMatch[1] : (currentGroup || 'Общие');
                     
                     channels.push({
-                        name: Utils.clearChannelName(name),
-                        original_name: name,
+                        name: name,
                         group: currentGroup,
                         logo: logoMatch ? logoMatch[1] : '',
-                        url: '' // Будет заполнено следующей строкой
+                        tvg_id: tvgIdMatch ? tvgIdMatch[1] : (tvgNameMatch ? tvgNameMatch[1] : ''),
+                        url: '' 
                     });
                 } else if (line.startsWith('http') || line.startsWith('rtmp')) {
                     if (channels.length > 0 && !channels[channels.length - 1].url) {
@@ -56,32 +48,30 @@
         }
     };
 
-    // 4. Основной компонент (Интерфейс)
+    // 3. Основной компонент
     function Component(object) {
         var comp = new Lampa.InteractionMain(object);
         var scroll;
         var channels_data = [];
+        var activeGroup = 'Все';
 
         comp.create = function () {
             var _this = this;
             this.activity.loader = true;
             
-            // Загрузка плейлиста напрямую
             var xhr = new XMLHttpRequest();
-            xhr.open('GET', playlist_config.url, true);
+            xhr.open('GET', config.url, true);
             xhr.onload = function () {
                 if (xhr.status === 200) {
                     channels_data = Parser.run(xhr.responseText);
                     _this.build();
                 } else {
                     Lampa.Noty.show('Ошибка загрузки плейлиста');
-                    _this.activity.loader = false;
                     Lampa.Controller.toggle('content');
                 }
             };
             xhr.onerror = function() {
                 Lampa.Noty.show('Ошибка сети');
-                _this.activity.loader = false;
                 Lampa.Controller.toggle('content');
             };
             xhr.send();
@@ -92,121 +82,128 @@
             var _this = this;
             this.activity.loader = false;
             
-            // Сбор уникальных групп
-            var groups = ['Всі'];
-            channels_data.forEach(function(c) {
-                if(c.group && groups.indexOf(c.group) === -1) groups.push(c.group);
-            });
-
-            // Создаем HTML вручную, имитируя оригинальный двухпанельный интерфейс
+            // Создаем структуру: Заголовок с кнопкой категорий и сетка контента
             var html = $(`
-                <div class="iptv-container">
-                    <div class="iptv-menu layer--wheight">
-                        <div class="iptv-menu__scroll"></div>
+                <div class="iptv-layout">
+                    <div class="iptv-header">
+                        <div class="iptv-title">${config.name}</div>
+                        <div class="iptv-filter selector">
+                            <svg style="width: 1.5em; height: 1.5em; vertical-align: middle; margin-right: 0.3em" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><line x1="3" y1="12" x2="21" y2="12"></line><line x1="3" y1="6" x2="21" y2="6"></line><line x1="3" y1="18" x2="21" y2="18"></line></svg>
+                            Категории
+                        </div>
                     </div>
-                    <div class="iptv-content layer--wheight">
-                        <div class="iptv-content__scroll"></div>
+                    <div class="iptv-list layer--wheight">
+                        <div class="iptv-list__scroll"></div>
                     </div>
                 </div>
             `);
+
+            this.html = html;
+            this.scroll_body = html.find('.iptv-list__scroll');
             
-            // Рендер левого меню (Группы)
-            var menu_scroll = html.find('.iptv-menu__scroll');
-            groups.forEach(function(group) {
-                var item = $('<div class="iptv-menu__item selector" data-group="'+group+'">'+group+'</div>');
-                item.on('hover:enter', function() {
-                    _this.setGroup(group, html.find('.iptv-content__scroll'));
-                });
-                menu_scroll.append(item);
+            // Логика скролла
+            scroll = new Lampa.Scroll({ mask: html.find('.iptv-list'), over: true, step: 250 });
+
+            // Обработчик кнопки категорий
+            html.find('.iptv-filter').on('hover:enter', function() {
+                _this.showCategories();
             });
 
-            // Инициализация скроллов
-            scroll = new Lampa.Scroll({ mask: html.find('.iptv-content'), over: true, step: 200 });
-            var menu_scroll_wrapper = new Lampa.Scroll({ mask: html.find('.iptv-menu'), over: true });
-
             this.activity.html.append(html);
-            menu_scroll_wrapper.init();
-
-            // Выбираем первую группу по умолчанию
-            this.setGroup('Всі', html.find('.iptv-content__scroll'));
+            this.setGroup('Все');
             
-            // Настройка контроллера (пульт)
+            // Назначаем контроллер
+            this.toggle();
+        };
+
+        comp.toggle = function() {
             Lampa.Controller.add('content', {
-                toggle: function () {
-                    Lampa.Controller.collectionSet(html);
+                toggle: () => {
+                    Lampa.Controller.collectionSet(this.html);
                     Lampa.Controller.mode('content');
                 },
-                left: function () {
-                    if (Lampa.Controller.collectionSet(html.find('.iptv-menu'))) {
-                        Lampa.Controller.mode('menu');
+                up: () => {
+                    if(!Lampa.Controller.up(this.scroll_body)) {
+                        // Если уперлись вверх списка - переходим на кнопку "Категории"
+                        Lampa.Controller.collectionSet(this.html.find('.iptv-header'));
                     }
                 },
-                right: function () {
-                    if (Lampa.Controller.collectionSet(html.find('.iptv-content'))) {
-                        Lampa.Controller.mode('content');
-                    }
+                down: () => {
+                    Lampa.Controller.down(this.scroll_body);
                 },
-                up: function () {
-                    if (Lampa.Controller.mode() === 'content') {
-                        if(!Lampa.Controller.up(html.find('.iptv-content'))) Lampa.Controller.toggle('menu');
-                    } else {
-                        Lampa.Controller.up(html.find('.iptv-menu'));
-                    }
+                left: () => {
+                    if(Lampa.Controller.mode() === 'content') Lampa.Controller.left(this.scroll_body);
                 },
-                down: function () {
-                    if (Lampa.Controller.mode() === 'content') {
-                        Lampa.Controller.down(html.find('.iptv-content'));
-                    } else {
-                        Lampa.Controller.down(html.find('.iptv-menu'));
-                    }
+                right: () => {
+                    if(Lampa.Controller.mode() === 'content') Lampa.Controller.right(this.scroll_body);
                 },
-                back: function () {
+                back: () => {
                     Lampa.Activity.back();
                 }
             });
-            
             Lampa.Controller.toggle('content');
         };
 
-        comp.setGroup = function(group, container) {
-            container.empty();
-            
-            var list = channels_data.filter(function(c) {
-                return group === 'Всі' || c.group === group;
-            });
+        comp.setGroup = function(group) {
+            activeGroup = group;
+            this.html.find('.iptv-title').text(group === 'Все' ? config.name : group);
+            this.scroll_body.empty();
 
-            list.forEach(function(channel) {
-                // Рендер элемента списка (Стиль Original Interface)
-                var logo = channel.logo ? '<img src="'+channel.logo+'" loading="lazy" />' : '<span>'+channel.name.slice(0,2)+'</span>';
+            var list = channels_data.filter(c => group === 'Все' || c.group === group);
+
+            // Рендер КАРТОЧЕК (как на скрине)
+            list.forEach(channel => {
+                // Если логотипа нет, генерируем заглушку с названием
+                var img = channel.logo ? `<img src="${channel.logo}" loading="lazy" />` : `<span>${channel.name}</span>`;
+                
                 var item = $(`
-                    <div class="iptv-channel selector">
-                        <div class="iptv-channel__ico">${logo}</div>
-                        <div class="iptv-channel__body">
-                            <div class="iptv-channel__title">${channel.name}</div>
-                            <div class="iptv-channel__epg">ТВ-программа недоступна (Local Mode)</div>
+                    <div class="iptv-card selector">
+                        <div class="iptv-card__body">
+                            <div class="iptv-card__img">${img}</div>
+                        </div>
+                        <div class="iptv-card__footer">
+                            <div class="iptv-card__title">${channel.name}</div>
+                            <div class="iptv-card__epg">Нет программы</div>
                         </div>
                     </div>
                 `);
-                
+
                 item.on('hover:enter', function() {
-                    var video = {
-                        url: channel.url,
-                        title: channel.name,
-                        author: 'IPTV Premium',
-                        source: 'iptv'
-                    };
+                    var video = { url: channel.url, title: channel.name, author: config.name, source: 'iptv' };
                     Lampa.Player.play(video);
-                    
-                    // Создаем плейлист для плеера
-                    var playlist = list.map(function(c){
-                        return { url: c.url, title: c.name, author: 'IPTV Premium' };
-                    });
+                    var playlist = list.map(c => ({ url: c.url, title: c.name }));
                     Lampa.Player.playlist(playlist);
                 });
-                container.append(item);
+
+                this.scroll_body.append(item);
             });
             
             scroll.init();
+            // Возвращаем фокус на первый элемент при смене группы
+            Lampa.Controller.collectionSet(this.scroll_body);
+        };
+
+        comp.showCategories = function() {
+            var groups = ['Все'];
+            channels_data.forEach(c => {
+                if(c.group && groups.indexOf(c.group) === -1) groups.push(c.group);
+            });
+
+            var items = groups.map(g => ({
+                title: g,
+                selected: g === activeGroup
+            }));
+
+            Lampa.Select.show({
+                title: 'Категории',
+                items: items,
+                onSelect: (a) => {
+                    this.setGroup(a.title);
+                },
+                onBack: () => {
+                    Lampa.Controller.toggle('content');
+                }
+            });
         };
 
         comp.render = function () {
@@ -214,40 +211,43 @@
         };
     }
 
-    // 5. Регистрация плагина
-    Lampa.Plugins.add('custom_iptv_premium', function () {
-        Lampa.Component.add('custom_iptv', Component);
-        
-        // Добавляем иконку на главный экран или в меню
+    // 4. Регистрация
+    Lampa.Plugins.add('custom_iptv_card', function () {
+        Lampa.Component.add('custom_iptv_card', Component);
         Lampa.Settings.main().push({
-            title: playlist_config.name,
-            component: 'custom_iptv',
+            title: config.name,
+            component: 'custom_iptv_card',
             icon: 'tv',
-            description: 'Локальный премиум плейлист'
+            description: 'Сетка каналов'
         });
     });
 
-    // 6. Стили (CSS) - Мимикрия под оригинальный интерфейс
+    // 5. CSS (Сетка карточек как на скрине)
     var style = document.createElement('style');
     style.innerHTML = `
-        .iptv-container { display: flex; height: 100%; background-color: #1b1b1b; }
-        .iptv-menu { width: 280px; background: rgba(0,0,0,0.3); display: flex; flex-direction: column; border-right: 1px solid rgba(255,255,255,0.05); }
-        .iptv-content { flex-grow: 1; padding: 20px; background: transparent; }
+        .iptv-layout { height: 100%; display: flex; flex-direction: column; }
+        .iptv-header { padding: 20px 40px; display: flex; justify-content: space-between; align-items: center; }
+        .iptv-title { font-size: 2em; font-weight: bold; }
+        .iptv-filter { padding: 10px 20px; background: rgba(255,255,255,0.1); border-radius: 50px; cursor: pointer; transition: all 0.3s; display: flex; align-items: center; font-size: 1.2em; }
+        .iptv-filter.focus { background: #fff; color: #000; transform: scale(1.1); }
         
-        .iptv-menu__item { padding: 14px 20px; color: #aaa; font-size: 1.1em; transition: all 0.2s; border-left: 4px solid transparent; }
-        .iptv-menu__item.focus { background: rgba(255,255,255,0.1); color: #fff; border-left-color: #fff; }
+        .iptv-list { flex-grow: 1; overflow: hidden; padding: 0 30px; }
+        .iptv-list__scroll { display: flex; flex-wrap: wrap; padding-bottom: 50px; }
         
-        .iptv-channel { display: flex; align-items: center; padding: 12px; margin-bottom: 8px; border-radius: 6px; background: rgba(255,255,255,0.03); transition: transform 0.2s; }
-        .iptv-channel.focus { background: #fff; color: #000; transform: scale(1.01); box-shadow: 0 4px 10px rgba(0,0,0,0.3); }
-        .iptv-channel__ico { width: 45px; height: 45px; background: #333; border-radius: 4px; overflow: hidden; display: flex; justify-content: center; align-items: center; margin-right: 15px; flex-shrink: 0; }
-        .iptv-channel__ico img { width: 100%; height: 100%; object-fit: contain; }
-        .iptv-channel__ico span { color: #fff; font-weight: bold; font-size: 1.2em; }
-        .iptv-channel.focus .iptv-channel__ico span { color: #000; }
+        .iptv-card { width: 180px; margin: 15px; position: relative; transition: transform 0.2s; }
+        .iptv-card.focus { transform: scale(1.1); z-index: 2; }
         
-        .iptv-channel__body { flex-grow: 1; overflow: hidden; }
-        .iptv-channel__title { font-size: 1.3em; font-weight: 500; white-space: nowrap; overflow: hidden; text-overflow: ellipsis; }
-        .iptv-channel__epg { font-size: 0.9em; opacity: 0.6; margin-top: 4px; }
-        .iptv-channel.focus .iptv-channel__epg { opacity: 0.8; color: #444; }
+        .iptv-card__body { width: 100%; aspect-ratio: 16/9; background: rgba(255,255,255,0.1); border-radius: 10px; overflow: hidden; display: flex; justify-content: center; align-items: center; position: relative; }
+        .iptv-card.focus .iptv-card__body { background: #fff; box-shadow: 0 0 20px rgba(0,0,0,0.5); }
+        
+        .iptv-card__img img { max-width: 80%; max-height: 80%; object-fit: contain; }
+        .iptv-card__img span { font-size: 1.5em; font-weight: bold; text-align: center; color: #aaa; padding: 10px; }
+        .iptv-card.focus .iptv-card__img span { color: #000; }
+        
+        .iptv-card__footer { padding-top: 10px; }
+        .iptv-card__title { font-size: 1.1em; font-weight: 500; white-space: nowrap; overflow: hidden; text-overflow: ellipsis; }
+        .iptv-card__epg { font-size: 0.8em; color: #aaa; margin-top: 3px; white-space: nowrap; overflow: hidden; text-overflow: ellipsis; }
+        .iptv-card.focus .iptv-card__epg { color: #ccc; }
     `;
     document.head.appendChild(style);
 
